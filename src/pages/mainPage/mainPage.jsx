9015ae6882672a45a0edcc5161bc3690750907bf
EnderKru <enderpro700@gmail.com>
2024-06-09 08:04:05 +0300

    directions without learn more

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -1,9 +1,10 @@
+import { Directions } from "../../components/directions/directions";
 
 
 export function MainPage(){
     return(
         <div className="main">
-            
+            <Directions/>
         </div>
     )
 }
