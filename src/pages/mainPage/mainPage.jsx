e2b3625708ad846851488801da1e297789e80042
EnderKru <enderpro700@gmail.com>
2024-06-09 08:25:00 +0300

    footer

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -1,10 +1,12 @@
 import { Directions } from "../../components/directions/directions";
+import { Footer } from "../../components/footer/footer";
 
 
 export function MainPage(){
     return(
         <div className="main">
             <Directions/>
+            <Footer/>
         </div>
     )
 }
